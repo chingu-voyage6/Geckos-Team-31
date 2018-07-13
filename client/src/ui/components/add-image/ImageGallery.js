53d723e8a2876e7fb2303aafb6b2d4016f340190
James Harding <j.harding@doopoll.co>
2018-07-13 13:48:30 +0100

    Added function for getting images from file and new files plus connected mongodb

--- client/src/ui/components/add-image/ImageGallery.js
+++ client/src/ui/components/add-image/ImageGallery.js
@@ -1,22 +1,47 @@
+import fetch from 'isomorphic-fetch';
 import React from 'react';
 import PropTypes from 'prop-types';
 import Header from '../_common/Header';
 
+class ImageGallery extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      gallery: [],
+    };
+  }
 
-const ImageGallery = (props) => {
-  const { gallery } = props;
-  return (
-    <div className="ImageGallery">
-      <Header
-        heading="Add images from gallery"
-        size="large"
-      />
-      <div className="ImageGallery--images">
-        {gallery.map(image => <img src={image} alt={image} key={image} />)}
+  componentDidMount() {
+    fetch('/api/images')
+      .then((response) => {
+        if (response.status >= 400) {
+          throw new Error('Bad response from server');
+        }
+        return response.json();
+      })
+      .then((response) => {
+        this.setState({
+          gallery: response,
+        })
+      })
+      .catch(error => console.log(error))
+  }
+
+  render() {
+    const { gallery } = this.state;
+    return (
+      <div className="ImageGallery">
+        <Header
+          heading="Add images from gallery"
+          size="large"
+        />
+        <div className="ImageGallery--images">
+          {gallery.map(image => <img src={`${image}`} alt={image} key={image} />)}
+        </div>
       </div>
-    </div>
-  );
+    );
 };
+}
 
 
 ImageGallery.propTypes = {
--- server.js
+++ server.js
@@ -1,14 +1,44 @@
-import express from 'express'
+const express = require('express')
+const path = require('path');
+const mongo = require('mongodb');
+const createError = require('http-errors');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const monk = require('monk');
+const db = monk('localhost:27017/user');
+const fs = require('fs');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = 5000;
 
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+
+app.use(function(req,res,next){
+    req.db = db;
+    next();
 });
 
-app.get('*', (req, res) => {
-  res.send({ something: 'Hello my friend' })
-})
+const imagePath = path.join(path.resolve(__dirname, 'client/src/assets'), '/images');
+
+app.use(express.static('client/src/assets/images'))
+
+function getDirectoryContent(req, res, next) {
+  fs.readdir(imagePath , function (err, images) {
+    if (err) { return next(err); }
+    res.locals.path = images;
+    next();
+  });
+}
+
+
+app.get('/api/images', getDirectoryContent, function(req, res) {
+  res.send(res.locals.path);
+});
+
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
