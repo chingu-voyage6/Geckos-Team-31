0482a8d642596d9a818e4ab8c94c528086010806
James Harding <j.harding@doopoll.co>
2018-08-11 11:55:17 +0100

    Continued refactoring log view and common components

--- client/src/ui/components/_common/Button.js
+++ client/src/ui/components/_common/Button.js
@@ -1,40 +1,57 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 const Button = ({
   label,
   onClick,
-  // className,
+  className,
+  icon,
   theme,
-  // isSubmit,
+  size,
   type,
-}) => (
-  <div className="Button">
+}) => {
+  const buttonClass = classNames('Button', {
+    'Button--success': theme === 'success',
+    'Button--warning': theme === 'warning',
+    'Button--link': theme === 'link',
+    'Button--highlight': theme === 'highlight',
+    'Button--medium': size === 'medium',
+    'Button--small': size === 'small',
+    'Button--icon': icon,
+    [className]: className,
+  });
+  return (
     <button
       type={type}
       onClick={onClick}
-      className={theme ? `Button--${theme}` : 'Button--list'}
+      className={buttonClass}
     >
-      {label}
+      <div className="Button__inner">
+        {icon || null}
+        {label}
+      </div>
     </button>
-  </div>
-);
+  );
+};
 
 Button.propTypes = {
   label: PropTypes.string,
   onClick: PropTypes.func,
-  // className: PropTypes.string,
+  className: PropTypes.string,
   theme: PropTypes.string,
-  // isSubmit: PropTypes.bool,
   type: PropTypes.string,
+  icon: PropTypes.string,
+  size: PropTypes.string,
 };
 Button.defaultProps = {
   label: undefined,
   onClick: undefined,
-  // className: undefined,
+  className: undefined,
   theme: undefined,
-  // isSubmit: undefined,
   type: undefined,
+  icon: undefined,
+  size: undefined,
 };
 
 export default Button;
