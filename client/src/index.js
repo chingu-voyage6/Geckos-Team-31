0e00f11ae4591a45e25b9b853c955bdf67f7659a
James Harding <j.harding@doopoll.co>
2018-08-17 09:50:37 +0100

    Altering packg.lock run scripts

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,5 @@ import ReactDOM from 'react-dom';
 import './styles/main.css';
 import './styles/_variables.scss';
 import App from './layout/App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
